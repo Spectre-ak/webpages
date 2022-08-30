a8bd3f4b3a5b403edef0d918bf9d9cb881d78df3
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2022-08-31 00:37:08 +0530

    Update App.js

--- gsoc-doc/src/App.js
+++ gsoc-doc/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save tofyhhk revhjmload.
+          Edit <code>src/App.js</code> and save tofyhhkuhg revhjmload.
         </p>
         <a
           className="App-link"
