64da60c26c4462ed84ced477bb6362e12ca62bbd
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2022-08-31 23:30:56 +0530

    main branch files

---
+++ welcome.php
@@ -0,0 +1,13 @@
+<html>
+   
+   <head>
+      <title>Hello World</title>
+   </head>
+   
+   <body>
+      Welcome <?php echo $_POST["name"]; ?><br>
+	  Your email address is: <?php echo $_POST["email"]; ?>
+
+   </body>
+
+</html>
