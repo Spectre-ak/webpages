f056eedb34510e20754814f3c44803409ef3579c
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2022-09-08 01:49:02 +0530

    Update content.js

--- gsoc-doc/src/content.js
+++ gsoc-doc/src/content.js
@@ -13,17 +13,33 @@ function CodeText(props) {
         <b><i>{props.txt}</i></b>
     )
 }
+
+function getEle(id){
+    return document.getElementById(id);
+}
+
+function GetHeader(props){
+    return(
+        <div>
+            <h4 id={props.id} className={"content-header"} onClick={()=>{getEle(props.id+"-link").click()}}>{props.heading}</h4>
+            <a id={props.id+"-link"} name={props.id} href={"#"+props.id}>{}</a>
+        </div>
+    )
+}
+
 function Content() {
     return (
         <div id="content">
             <TopNavbar />
 
-            <h3>Project Description</h3>
+            <GetHeader id = "project-description" heading="Project Description"/>
+            
             <p>The Virtual Brain (TVB) is a brain stimulation platform. It offers tools for computing, simulating and analyzing functional and structural data of human brains. TVB provides a graphical user interface (GUI) for using its tools for studying human brains. The platform supports importing data into TVB like projects, data structures etc. Since TVB provides various tools for studying human brains the platform is also accessible through REST APIs. Goal of this project is to create a new package which will consume TVB REST services and perform operations on it. The new python client will have features like uploading data into a TVB, data encryption and validation, build BIDS datasets, monitor directories etc. Users will be able to use this python client to interact with a remote TVB server without using an active GUI.</p>
 
+            <p>Mentors: <ViewCode content="@Paula Popa" href="https://github.com/popaula937"/>&nbsp;<ViewCode content="@Lia Domide" href="https://github.com/liadomide"/></p>
             <div className="line"></div>
 
-            <h3>Milestone 1 - Update BIDS Importer</h3>
+            <GetHeader id="update-bids-importer" heading="Milestone 1 - Update BIDS Importer"/>
             <p>
                 In this phase we started working on updating the BIDS Importer. BIDS impoter is used to import bids datasets into TVB projcts.
                 Goal was to modify the bids importer so that it will also support bids datasets with only single datatype (ts, net, coords, spatial). We're able to achieve this and we also tested the updated bids importer with BIDS datasets containing only time series or connectivity datatypes.
@@ -34,7 +50,7 @@ function Content() {
             </p>
             <div className="line"></div>
 
-            <h3>Milestone 2 - Creating BIDS data builder</h3>
+            <GetHeader heading="Milestone 2 - Creating BIDS data builder" id="bids-data-builder"/>
             <p>In the second phase the goal was to build a module which can create datasets from a BIDS directory. The need was to
                 design a builder which will create datasets with only required files and no other files. This will also reduce the
                 size of the datasets.
@@ -57,7 +73,7 @@ function Content() {
             
             <div className="line"></div>
 
-            <h3>Milestone 3 - Creating BIDS Diretory monitor</h3>
+            <GetHeader id="bids-dir-watcher" heading="Milestone 3 - Creating BIDS Diretory monitor"/>
             <p>
                 After building <CodeText txt="BIDSDataBuilder" /> our next goal was to wtite a separate module for observing
                 or monitoring a BIDS directory for new files and whenever new files are added then consume <CodeText txt="BIDSDataBuilder " />
@@ -95,7 +111,8 @@ function Content() {
 
             <div className="line"></div>
 
-            <h3>Milestone 4 - Testing</h3>
+            <GetHeader heading="Milestone 4 - Testing" id="testing"/> 
+
             <p>
                 Now that we have <CodeText txt="BIDSDirWatcher"/> and <CodeText txt="BIDSDataBuilder" /> as a next step we need
                 to test the module properly.
@@ -116,7 +133,7 @@ function Content() {
 
             <div className="line"></div>
 
-            <h3>Milestone 5 - Packaging the code</h3>
+            <GetHeader id="code-packaging" heading="Milestone 5 - Packaging the code"/>
             <p>
                 This is last stage of the projet which inlcludes packaging the whole code with all dependencies so that it can be installed easily.
             </p>
@@ -129,13 +146,29 @@ function Content() {
                     <ViewCode content="MANIFEST" href="https://github.com/Spectre-ak/tvb-root/blob/tvb-bids-monitoring-gsoc/tvb_framework/MANIFEST_bids_monitor.in"/>
                 </li>
                 <li>
-                    Code documentation <ViewCode content="Readme" href="https://github.com/Spectre-ak/tvb-root/blob/tvb-bids-monitoring-gsoc/tvb_framework/tvb/interfaces/rest/bids_monitoring/README.md"/>
+                    README <ViewCode content="Readme" href="https://github.com/Spectre-ak/tvb-root/blob/tvb-bids-monitoring-gsoc/tvb_framework/tvb/interfaces/rest/bids_monitoring/README.md"/>
                 </li>
             </ul>
 
 
             <div className="line"></div>
 
+            <GetHeader heading="PRs and code repositries" id="links"/>
+
+            <ul>
+                <li>
+                    Pull Request: <ViewCode content="(GSoC 2022, 8.2) TVB BIDS data builder and dir monitor module #589" href="https://github.com/the-virtual-brain/tvb-root/pull/589"/>
+                </li>
+                <li>
+                    TVB main repo:  <ViewCode content="the-virtual-brain/tvb-root" href="https://github.com/the-virtual-brain/tvb-root"/>
+                </li>
+                <li>
+                    Forked repo: <ViewCode content="Spectre-ak/tvb-root, tvb-bids-monitoring-gsoc" href="https://github.com/Spectre-ak/tvb-root/tree/tvb-bids-monitoring-gsoc"/>
+                </li>
+                <li>
+                    Code documentation: <ViewCode content="README.md" href="https://github.com/Spectre-ak/tvb-root/blob/tvb-bids-monitoring-gsoc/tvb_framework/tvb/interfaces/rest/bids_monitoring/README.md"/>
+                </li>
+            </ul>
         </div>
     )
 }
